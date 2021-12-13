5aa1122d1962a3337a207e5bb0f6ee8eb8dd06f4
Thigo De Morais <demoraisthiago63@gmail.com>
2021-12-13 11:34:53 -0300

    general refactoring

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
-let formulary = document.querySelector(".quiz-form");
-let rightAnswers = ["B", "B", "B", "B"];
-let responsesReceived = [];
-let score = 0;
+const popupWrapper = document.querySelector(".popup-wrapper");
+const h1 = document.querySelector(".score-text");
+const form = document.querySelector(".quiz-form");
+const rightAnswers = ["D", "C", "D", "B"];
+const reinitButton = document.querySelector(".reinit-button");
 
-function showScore(score = 0) {
-  const popupWrapper = document.querySelector(".popup-wrapper");
-  const h1 = document.querySelector(".score-text");
 
-  popupWrapper.style.display = "block";
-  h1.textContent = "Pontuação: " + score;
+function reinitButtonVisibility() {
+  reinitButton.style.visibility = "visible";
 }
 
-function scoreCalculation() {
-  responsesReceived.forEach((element, index) => {
-    if (element === rightAnswers[index]) {
+function calculateScore(array) {
+  let score = 0;
+  array.forEach((rightAnswer, index) => {
+    if (rightAnswer === rightAnswers[index]) {
       score += 25;
     }
   });
   return score;
 }
 
-formulary.addEventListener("submit", (event) => {
+function showScore(score = 0) {
+  popupWrapper.style.display = "block";
+  let animationScore = 0;
+  const count = setInterval(() => {
+    if (animationScore === score) {
+      reinitButtonVisibility();
+      clearInterval(count);
+      
+    }
+    h1.textContent = "Pontuação: " + `${animationScore++}/100`;
+    
+  }, 50);
+}
+
+function getReferencies(array) {
+  rightAnswers.forEach((rightAnswer, index) =>{
+    const alternativeName = `inputQuestion${index + 1}`
+    array.push(form[alternativeName].value);
+  })
+}
+
+const mainCallback = (event) => {
+  const userAnswers  = [];
   event.preventDefault();
+  getReferencies(userAnswers );
+  const score = calculateScore(userAnswers );
+  showScore(score);
+};
 
-  responsesReceived.push(
-    event.target.inputQuestion1.value,
-    event.target.inputQuestion2.value,
-    event.target.inputQuestion3.value,
-    event.target.inputQuestion4.value
-  );
+const reinitCallback = () => {
+  popupWrapper.style.display = "none";
+  reinitButton.style.visibility = "hidden";
+};
 
-  showScore(scoreCalculation());
-});
+reinitButton.addEventListener("click", reinitCallback);
+form.addEventListener("submit", mainCallback);
